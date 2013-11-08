6be4375753d17dba4778c310c4184fa23aecc4eb
Justin <macusers@Justins-MacBook-Pro.local>
2013-11-07 18:44:51 -0800

    first version

---
+++ index.php
@@ -0,0 +1,313 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Team Fast Agents | Listing</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600" rel="stylesheet" type="text/css" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<link rel="stylesheet" href="/css/jquery.fancybox.css" type="text/css" media="screen" />
+		<script type="text/javascript" src="/js/jquery.fancybox.pack.js"></script>
+		<script type="text/javascript">
+		$(document).ready(function()
+		{	
+			$('.fancybox').fancybox( { closeClick : 'true' } );
+		});
+		</script>
+
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-wide.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<div id="header" class="skel-panels-fixed">
+
+				<div class="top">
+
+					<!-- Logo -->
+						<div id="logo">
+							<span class="image avatar48"><img src="images/teamfastagenticon.png" alt="" /></span>
+							<h1 id="title">Team Fast Agents</h1>
+							<span class="byline">YOUR personal team of professionals</span>
+						</div>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li><a href="#top" id="top-link" class="skel-panels-ignoreHref"><span class="icon icon-home">Intro</span></a></li>
+								<li><a href="#portfolio" id="portfolio-link" class="skel-panels-ignoreHref"><span class="icon icon-th">Listing Services</span></a></li>
+								<li><a href="#about" id="about-link" class="skel-panels-ignoreHref"><span class="icon icon-dollar">Market Analysis</span></a></li>
+								<li><a href="#contact" id="contact-link" class="skel-panels-ignoreHref"><span class="icon icon-group">About Us</span></a></li>
+								<li><a href="#contact" id="contact-link" class="skel-panels-ignoreHref"><span class="icon icon-phone">Call Kenny - 510.545.6932</span></a></li>
+								<li><a href="#contact" id="contact-link" class="skel-panels-ignoreHref"><span class="icon icon-phone">Call Kevin - 510.995.0015</span></a></li>
+							</ul>
+						</nav>
+						
+				</div>
+				
+				<div class="bottom">
+
+					<!-- Social Icons -->
+						<ul class="icons">
+							<li><a href="https://twitter.com/kenny_truong" class="icon icon-twitter"><span>Twitter</span></a></li>
+							<li><a href="https://www.facebook.com/FastAgent?fref=ts" class="icon icon-facebook"><span>Facebook</span></a></li>
+							<li><a href="https://www.youtube.com/channel/UCF9s0fFFZu9W0v6Z3VwyRIA" class="icon icon-youtube"><span>Youtube</span></a></li>
+							<li><a href="mailto:info@teamfastagents.com" class="icon icon-envelope"><span>Email</span></a></li>
+						</ul>
+				
+				</div>
+			
+			</div>
+
+		<!-- Main -->
+			<div id="main">
+			
+				<!-- Intro -->
+					<section id="top" class="one">
+						<div class="container">
+
+							<img class="headerlogo" src="images/teamfastagents.jpg" alt="" />
+
+							<header>
+								<h2 class="alt">Thinking about selling <span class="blackblock"> 737 2nd St #403? </span> Let us help! This is YOUR personal team of professionals. Whether you have questions about purchasing or selling, you can rest assured when you have your own Team of Fast Agents backing you.</h2>
+							</header>
+
+							<footer>
+								<a href="#portfolio" class="button scrolly">More info</a>
+							</footer>
+
+						</div>
+					</section>
+					
+				<!-- Portfolio -->
+					<section id="portfolio" class="two">
+						<div class="container">
+							<header>
+								<h2 class="blackblock">Listing Services</h2>
+							</header>
+							
+							<p>When you list your home with Team Fast Agents, you receive a premium marketing plan with a personal touch.</p>
+						
+							<div class="row">
+								<div class="4u">
+									<article class="item">
+										<a href="images/home.jpg" class="image full fancybox"><img src="images/home.jpg" alt="" /></a>
+										<header>
+											<h3><strong>Custom Internet Domain</strong></h3>
+										</header>
+									</article>
+									<article class="item">
+										<a href="#" class="image full"><iframe class="image full" src="http://www.haikudeck.com/e/Mb0O6AZ7Pa" height="301" width="640" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe></a>
+										<header>
+											<h3>The latest presentation solutions</h3>
+										</header>
+									</article>
+									<article class="item">
+										<a href="images/pic06.jpg" class="image full fancybox"><img src="images/pic06.jpg" alt="" /></a>
+										<header>
+											<h3>Fast Agent branding</h3>
+										</header>
+									</article>
+								</div>
+								<div class="4u">
+									<article class="item">
+										<a href="images/home.jpg" class="image full fancybox"><img src="images/cam2.jpg" alt="" /></a>
+										<header>
+											<h3>Professional Photography</h3>
+										</header>
+									</article>
+									<article class="item">
+										<a href="images/printad2.jpg" class="image full fancybox"><img src="images/printad2.jpg" alt="" /></a>
+										<header>
+											<h3>Print ads in multiple leading Vietnamese and Chinese language magazines</h3>
+
+										</header>
+									</article>
+									<article class="item">
+										<a href="images/flyer.jpg" class="image full fancybox"><img src="images/flyer.jpg" alt="" /></a>
+										<header>
+											<h3><strong>Modern Designs</strong></h3>
+										</header>
+									</article>
+								</div>
+								<div class="4u">
+									<article class="item">
+										<a href="images/postcard.jpg" class="image full fancybox"><img src="images/postcard.jpg" alt="" /></a>
+										<header>
+											<h3><strong>Direct Mail Campaigns</strong></h3>
+										</header>
+									</article>
+									<article class="item">
+										<iframe class="image full" width="560" height="315" src="//www.youtube.com/embed/aAs99g4kTMA" frameborder="0" allowfullscreen></iframe>
+										<header>
+											<h3>Video tours</h3>
+										</header>
+									</article>
+									<article class="item">
+										<a href="images/smartryder.png" class="image full fancybox"><img src="images/smartryder.png" alt="" /></a>
+										<header>
+											<h3><strong>Innovative Signage</strong></h3>
+										</header>
+									</article>
+								</div>
+							</div>
+							<br>
+							<a href="http://www.redfin.com/real-estate-agents/kennyt?show=reviews" target="_blank" class="image full"><img style="width: 100%;" src="images/redfin review.jpg" /></a>
+							<br>
+							<a href="images/youtube.jpg" class="image full fancybox"><img style="width: 100%;" src="images/youtube.jpg" /></a>
+						</div>
+					</section>
+
+				<!-- About Me -->
+					<section id="about" class="three">
+						<div class="container">
+							<header>
+								<h2 style="color: #fff;">Comparative Market Analysis</h2>
+								<h3 style="color: #ecf0f1;">Properties sold within area of 737 2nd St #403</h3>
+							</header>
+							<div class="row">
+								<div class="4u">
+									<article class="item">
+										<a href="images/comp1.jpg" class="image full fancybox"><img src="images/comp1.jpg" alt="" /></a>
+										<header>
+										<h3><strong>5312 Walnut St</strong></h3>
+										<ul>
+											<li>2 Bed - 1 Bath</li>
+											<li>1,130 Sq Ft -  0 Lot</li>
+											<li>List: 8/20/13 - Pending: 8/29/13</li>
+											<li>List: $399,000 - SOLD $425,000</li>
+										</ul>
+									</article>
+								</div>
+								<div class="4u">
+									<article class="item">
+										<a href="images/comp2.jpg" class="image full fancybox"><img src="images/comp2.jpg" alt="" /></a>
+										<header>
+										<h3><strong>2715 55th Ave</strong></h3>
+										<ul>
+											<li>2 Bed - 1 Bath</li>
+											<li>1,249 Sq Ft - 0 Lot</li>
+											<li>List: 8/9/2013 - Pending: 8/26/13</li>
+											<li>List: $349,000 - SOLD $371,000</li>
+										</ul>
+									</article>
+								</div>
+								<div class="4u">
+									<article class="item">
+										<a href="images/comp3.jpg" class="image full fancybox"><img src="images/comp3.jpg" alt="" /></a>
+										<header>
+										<h3><strong>3435 Kingsland Ave</strong></h3>
+										<ul>
+											<li>2 Bed - 1 Bath</li>
+											<li>800 Sq Ft - 0 Lot</li>
+											<li>List: 8/24/13 - Pending: 9/3/13</li>
+											<li>List: $398,000 - SOLD $420,000</li>
+										</ul>
+									</article>
+								</div>
+							</div>
+							<br />
+							<a href="images/compdata.png" class="image full fancybox"><img style="width: 100%;" src="images/compdata.png" /></a>
+						</div>
+					</section>
+			
+				<!-- Contact -->
+					<section id="contact" class="four">
+						<div class="container">
+
+						<img src="images/mjre.jpg" alt="" class="image featured"/>
+						<header>
+							<h2>About Team Fast Agents</h2>
+							<span class="byline">Award winning team members with millions in properties sold.</span>
+						</header>
+						<div class="row">
+						
+						<article class="4u">
+							<img class="circled" src="images/KennyLogo.jpg" alt="" />
+							<header>
+								<h3>Kenny Truong</h3>
+								<span class="small">Agent</span><br>
+								<span class="small">510.545.6932</span><br>
+								<a href="mailto:kenny@fastagent.me"><span class="small">kenny@fastagent.me</span></a>
+							</header>
+							<p>
+								Kenny is currently in the top 0.5% of units closed out of 20,000+ Agents in our local market. He is a branded #fastagent who has inspired many in Alameda counties Real Estate industry with his cutting edge visions.
+							</p>
+						</article>
+						<article class="4u ">
+							<img class="circled" src="images/klowelogo.jpg" alt="" />
+							<header>
+								<h3>Kevin Lowe</h3>
+								<span class="small">Agent</span><br>
+								<span class="small">510.851.5454</span><br>
+								<a href="mailto:kevin.mjre@gmail.com"><span class="small">kevin.mjre@gmail.com</span></a>
+							</header>
+							<p>
+								Kevin is a dedicated professional who will go above and beyond his due dilegence as an agent to insure his client's buying or selling experience is as streamlined as possible.
+							</p>
+						</article>
+						<article class="4u ">
+							<img class="circled" src="images/blanklogo.jpg" alt="" />
+							<header>
+								<h3>David Nguyen</h3>
+								<span class="small">Marketing | Web</span><br>
+								<span class="small">510.922.0161</span><br>
+								<a href="mailto:david.mjre@gmail.com"><span class="small">david.mjre@gmail.com</span></a>
+							</header>
+							<p>
+								David is in charge of Marketing and Web development for Team Fast Agents. He keeps up with the latest trends and implements new designs to insure the team is always cutting edge.
+							</p>
+						</article>
+					</div>
+
+							
+							
+							<!-- <form method="post" action="#">
+								<div class="row half">
+									<div class="6u"><input type="text" class="text" name="name" placeholder="Name" /></div>
+									<div class="6u"><input type="text" class="text" name="email" placeholder="Email" /></div>
+								</div>
+								<div class="row half">
+									<div class="12u">
+										<textarea name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="row">
+									<div class="12u">
+										<a href="#" class="button submit">Send Message</a>
+									</div>
+								</div>
+							</form> -->
+
+						</div>
+					</section>
+			
+			</div>
+
+		<!-- Footer -->
+			<div id="footer" style="background-color: #2c3e50; color: #000;">
+				
+				<!-- Copyright -->
+					<div class="copyright">
+						<p>&copy; 2013 Team Fast Agents. All rights reserved.</p>
+						<ul class="menu">
+							<li>by <a href="http://devdavid.com">DevDavid.com</a></li>
+						</ul>
+					</div>
+				
+			</div>
+
+	</body>
+</html>
