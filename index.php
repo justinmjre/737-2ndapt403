6e32fdac32c5d2a03006a672975331be9e5db765
Justin <macusers@Justins-MacBook-Pro.local>
2013-11-07 19:57:26 -0800

    no cma button

--- index.php
+++ index.php
@@ -48,7 +48,6 @@
 							<ul>
 								<li><a href="#top" id="top-link" class="skel-panels-ignoreHref"><span class="icon icon-home">Intro</span></a></li>
 								<li><a href="#portfolio" id="portfolio-link" class="skel-panels-ignoreHref"><span class="icon icon-th">Listing Services</span></a></li>
-								<li><a href="#about" id="about-link" class="skel-panels-ignoreHref"><span class="icon icon-dollar">Market Analysis</span></a></li>
 								<li><a href="#contact" id="contact-link" class="skel-panels-ignoreHref"><span class="icon icon-group">About Us</span></a></li>
 								<li><a href="#contact" id="contact-link" class="skel-panels-ignoreHref"><span class="icon icon-phone">Call Kenny - 510.545.6932</span></a></li>
 								<li><a href="#contact" id="contact-link" class="skel-panels-ignoreHref"><span class="icon icon-phone">Call Kevin - 510.995.0015</span></a></li>
